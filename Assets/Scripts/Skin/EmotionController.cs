a2497414568e1f61aaaea0c609446d49ed48baf8
n.fontes <n.fontes@student.isartdigital.com>
2017-11-19 05:53:40 +0100

    Update all animations

---
+++ Assets/Scripts/Skin/EmotionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EmotionController : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        GameManager.Instance.OnEmotionChanged += this.Instance_OnEmotionChanged;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.OnEmotionChanged -= Instance_OnEmotionChanged;
+    }
+
+    private void Instance_OnEmotionChanged(CharacterEmotion emo)
+    {
+        GetComponent<Animator>().SetTrigger((int) emo);
+    }
+}
